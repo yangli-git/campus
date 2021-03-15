a2bd4da145bac3afc143e7e448ab8597a41d52b7
yangli <yangli@stargraph.cn>
2021-03-15 19:06:25 +0800

    部分文件格式化  添加sql 包含基本数据

--- campushelp/src/main/resources/campus.sql
+++ campushelp/src/main/resources/campus.sql
@@ -1,24 +1,26 @@
 /*
-SQLyog Ultimate v11.33 (64 bit)
-MySQL - 5.7.23 : Database - campushelp
-*********************************************************************
+ Navicat Premium Data Transfer
+
+ Source Server         : school
+ Source Server Type    : MySQL
+ Source Server Version : 50733
+ Source Host           : localhost:3306
+ Source Schema         : campus
+
+ Target Server Type    : MySQL
+ Target Server Version : 50733
+ File Encoding         : 65001
+
+ Date: 14/03/2021 00:36:50   含有数据
 */
 
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-USE `campushelp`;
-
-/*Table structure for table `admin` */
-
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
 DROP TABLE IF EXISTS `admin`;
-
 CREATE TABLE `admin` (
   `aId` int(6) NOT NULL AUTO_INCREMENT,
   `account` varchar(255) NOT NULL,
@@ -26,13 +28,20 @@ CREATE TABLE `admin` (
   `name` varchar(255) NOT NULL DEFAULT 'name',
   `addTime` datetime(6) NOT NULL,
   `state` int(2) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`aid`)
+  PRIMARY KEY (`aId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
-/*Table structure for table `school` */
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+BEGIN;
+INSERT INTO `admin` VALUES (1, 'yangli', '123456', 'yangli', '2021-03-14 00:32:00.000000', 0);
+COMMIT;
 
+-- ----------------------------
+-- Table structure for school
+-- ----------------------------
 DROP TABLE IF EXISTS `school`;
-
 CREATE TABLE `school` (
   `schoolId` int(5) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT 'schoolName',
@@ -41,10 +50,18 @@ CREATE TABLE `school` (
   PRIMARY KEY (`schoolId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
-/*Table structure for table `task` */
+-- ----------------------------
+-- Records of school
+-- ----------------------------
+BEGIN;
+INSERT INTO `school` VALUES (1, '武汉轻工大学', '2021-03-14 00:35:49.000000', 0);
+INSERT INTO `school` VALUES (2, '武汉理工大学', '2021-03-14 00:36:06.000000', 0);
+COMMIT;
 
+-- ----------------------------
+-- Table structure for task
+-- ----------------------------
 DROP TABLE IF EXISTS `task`;
-
 CREATE TABLE `task` (
   `taskId` int(8) NOT NULL AUTO_INCREMENT,
   `publish_user_id` varchar(255) NOT NULL COMMENT '发布用户学号',
@@ -56,14 +73,14 @@ CREATE TABLE `task` (
   `endTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   `taskName` varchar(255) NOT NULL COMMENT '任务名称',
   `taskContext` varchar(255) NOT NULL COMMENT '任务描述',
-  `state` int(2) NOT NULL DEFAULT '0' COMMENT '任务状态',
-  PRIMARY KEY (`taskid`)
+  `state` int(2) NOT NULL DEFAULT '0' COMMENT '任务状态,-1暂存 0 已完成 1 解决中 2 待解决',
+  PRIMARY KEY (`taskId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
-/*Table structure for table `user` */
-
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
 DROP TABLE IF EXISTS `user`;
-
 CREATE TABLE `user` (
   `stuId` int(11) NOT NULL AUTO_INCREMENT,
   `studentId` varchar(20) NOT NULL DEFAULT '' COMMENT '学号',
@@ -74,10 +91,16 @@ CREATE TABLE `user` (
   `registerTime` datetime(6) NOT NULL COMMENT '注册时间',
   `money` double(20,0) NOT NULL DEFAULT '0',
   `state` int(2) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`stuid`)
+  PRIMARY KEY (`stuId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` VALUES (1, '1705110115', '123456', 1, 0, '张三', '2021-03-14 00:34:21.000000', 20, 0);
+INSERT INTO `user` VALUES (2, '1705110116', '123456', 1, 0, '李四', '2021-03-14 00:35:02.000000', 20, 0);
+INSERT INTO `user` VALUES (3, '1705110117', '123456', 2, 0, '王五', '2021-03-14 00:35:28.000000', 0, 0);
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
